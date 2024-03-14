dada12092495c41b24b766e735213fb0cecc88c2
Santiago Mosquera <132929561+Onixdrew@users.noreply.github.com>
2024-03-13 21:01:13 -0500

    Update inputTabla.jsx

--- src/ComponentesEntregable/inputTabla.jsx
+++ src/ComponentesEntregable/inputTabla.jsx
@@ -21,19 +21,19 @@ const InputTabla = ({ onBuscarChange, opcionApp }) => {
 
   return (
     <div className="flex">
-      <FaSearch className="lg:ml-[10.5vw] sm:ml-[11vw] sm:mt-[0.6vw]  lg:mt-[0.5vh] text-lg absolute z-10 " />
+      <FaSearch className="lg:ml-[1vw] md:ml-2 lg:mt-[0.8vh] sm:ml-[1.5vw] sm:mt-[0.6vw]  lg:mt-[0.5vh] text-lg absolute z-10 " />
       <input
         type="text"
         placeholder="Buscar"
         value={buscar}
         onChange={actualizarBuscar}
-        className="border-2 lg:pl-[2vw] hover:border-green-500 sm:pl-[5vw] relative border-black rounded-xl pl-3 ml-[10vw] mb-[3vh]"
+        className="border-2 lg:pl-[3vw] hover:border-green-500 sm:pl-[5vw] relative border-black rounded-xl pl-3 sm:ml-[vw] mb-[3vh]"
       />
 
      <div className="flex flex-col  sm:mt-[-3.5vh] md:mt-[-3.5vh] md:ml-[-1vw]">
         <label htmlFor="opciones" className="sm:ml-3 md:ml-[2vw]">Cantidad</label>
         <select id="opciones" className="bg-gray-400 sm:ml-[2vw] rounded px-3" onChange={obtenerOpcion}>
-          <option value="" selected></option>
+          <option value="0"></option>
           <option value="5">5</option>
           <option value="10">10</option>
           <option value="20">20</option>
@@ -45,3 +45,4 @@ const InputTabla = ({ onBuscarChange, opcionApp }) => {
 };
 
 export default InputTabla;
+
